93c876168218fb5e7b8b0c0f19babfbd4cb484c0
Rodgers Chayuga <chayugarodgers@gmail.com>
2025-01-03 16:54:53 +0300

    deployment: Deploy smart contract to alchemy (Sepolia Testnet)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// Ensure your configuration variables are set before executing the script
+const { vars } = require("hardhat/config");
+
+// Go to https://alchemy.com, sign up, create a new App in
+// its dashboard, and add its key to the configuration variables
+const API_KEY = process.env.ALCHEMY_API_KEY;
+
+// Add your Sepolia account private key to the configuration variables
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${API_KEY}`,
+      accounts: [PRIVATE_KEY],
+      gas: 2100000,  // Add this line
+      gasPrice: 8000000000  // Add this line (8 gwei)
+    }
+  }
 };
